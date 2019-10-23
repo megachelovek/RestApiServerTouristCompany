73f7766dcf52ba0473b10b4981eb37793b66c792
megachelovek <108danila108@rambler.ru>
2019-10-23 13:23:44 +0400

    [+] #master Создания простого API

---
+++ ServerTouristCompanyApi/SwaggerExamples/FooListResponseExample.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using ServerTouristCompanyApi.Models;
+using Swashbuckle.AspNetCore.Filters;
+
+namespace ServerTouristCompanyApi.SwaggerExamples
+{
+    public class FooListResponseExample : IExamplesProvider
+    {
+        public object GetExamples()
+        {
+            return new List<Foo>
+            {
+                new Foo { Id = new Random().Next(), Value = Guid.NewGuid().ToString().Remove(6)},
+                new Foo { Id = new Random().Next(), Value = Guid.NewGuid().ToString().Remove(6)}
+            };
+        }
+    }
+}
---
+++ ServerTouristCompanyApi/SwaggerExamples/FooResponseExample.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using ServerTouristCompanyApi.Models;
+using Swashbuckle.AspNetCore.Filters;
+
+namespace ServerTouristCompanyApi.SwaggerExamples
+{
+    public class FooResponseExample : IExamplesProvider
+    {
+        public object GetExamples()
+        {
+            return new Foo
+            {
+                Id = new Random().Next(),
+                Value = Guid.NewGuid().ToString().Remove(6)
+            };
+        }
+    }
+}
